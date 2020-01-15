cf696cfb091e89be060a98b0da1db6e0539c4767
JustinBarsalou <justin.barsalou@g.austincc.edu>
2020-01-15 16:03:48 -0600

    cant get it to work, but i am not sure whats wrong

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [],
+    }
+
+  }
+
+  componentDidMount() {
+    fetch('https://api.punkapi.com/v2/')
+    .then(res => res.json())
+  //  .then(parsedJson => console.log(parsedJson.result))
+    .then(data => this.setState({posts: data}))
+  }
+
+
+  render() {
+    return (
+      <div>
+    {this.state.posts.map((post) => {
+      return <div key={post.id}>
+      <h2>{post.name}</h2>
+      <p>{post.tagline}</p>
+      </div>
+    })}
     </div>
-  );
+    )
+  }
 }
 
+
 export default App;
+
